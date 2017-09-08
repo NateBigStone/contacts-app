b01b6f2969ffd91071a2545a47a4111e6f31f038
NateBigStone <n.gaut@bigstonemgmt.com>
2017-09-07 20:10:21 -0500

    Contacts App

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,7 @@ class ContactsController < ApplicationController
 	def index
 		@contact = Contact.all 
 	end	
+  def show
+    @contact = Contact.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/" => "contacts#index"
+  get "/contacts/new" => "contacts#new"
+  post "/contacts" => "contacts#create"
+  get "/contacts/:id" => "contacts#show"
 end
 
